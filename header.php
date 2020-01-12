caedfa2be53cd27d59c08e912b126ffcdf5c37e3
tomasmparra <tomas.mparra@lattedev.com>
2020-01-12 14:45:33 +0100

    HEADER First steps

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 <body>
   <header class="header loading" id="header">
     <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo" class="logo">
-    <div class="upperNav">
+    <nav class="upperNav">
       <div class="socialMedia">
         <a href="" class="socialMediaLink socialMediaInst">
           <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
@@ -37,15 +37,66 @@
           </svg>
         </a>
       </div>
-      <a href="" class="mail">motorsport@amatumoto.com</a>
-      <a href="" class="langButton">Europe|Spain</a>
-      <a href="" class="langButton">USA</a>
-    </div>
-    <div class="navBar">
+
+      <a href="" class="mail upperNavInfo">
+        <svg class="upperNavIcon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+        <p>motorsport@amatumoto.com</p>
+      </a>
+      <a href="" class="langButton upperNavInfo">
+        <svg class="upperNavIcon" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+        <circle style="fill:#0052B4;" cx="256" cy="256" r="256"/>
+        <g>
+        	<polygon style="fill:#FFDA44;" points="256.001,100.174 264.29,125.683 291.11,125.683 269.411,141.448 277.7,166.957
+        		256.001,151.191 234.301,166.957 242.59,141.448 220.891,125.683 247.712,125.683 	"/>
+        	<polygon style="fill:#FFDA44;" points="145.814,145.814 169.714,157.99 188.679,139.026 184.482,165.516 208.381,177.693
+        		181.89,181.889 177.694,208.381 165.517,184.482 139.027,188.679 157.992,169.714 	"/>
+        	<polygon style="fill:#FFDA44;" points="100.175,256 125.684,247.711 125.684,220.89 141.448,242.59 166.958,234.301 151.191,256
+        		166.958,277.699 141.448,269.411 125.684,291.11 125.684,264.289 	"/>
+        	<polygon style="fill:#FFDA44;" points="145.814,366.186 157.991,342.286 139.027,323.321 165.518,327.519 177.693,303.62
+        		181.89,330.111 208.38,334.307 184.484,346.484 188.679,372.974 169.714,354.009 	"/>
+        	<polygon style="fill:#FFDA44;" points="256.001,411.826 247.711,386.317 220.891,386.317 242.591,370.552 234.301,345.045
+        		256.001,360.809 277.7,345.045 269.411,370.552 291.11,386.317 264.289,386.317 	"/>
+        	<polygon style="fill:#FFDA44;" points="366.187,366.186 342.288,354.01 323.322,372.975 327.519,346.483 303.622,334.307
+        		330.112,330.111 334.308,303.62 346.484,327.519 372.974,323.321 354.009,342.288 	"/>
+        	<polygon style="fill:#FFDA44;" points="411.826,256 386.317,264.289 386.317,291.11 370.552,269.41 345.045,277.699 360.81,256
+        		345.045,234.301 370.553,242.59 386.317,220.89 386.317,247.712 	"/>
+        	<polygon style="fill:#FFDA44;" points="366.187,145.814 354.01,169.714 372.975,188.679 346.483,184.481 334.308,208.38
+        		330.112,181.889 303.622,177.692 327.519,165.516 323.322,139.027 342.289,157.991 	"/>
+        </g>
+        </svg>
+        <p>Europe|Spain</p>
+      </a>
+      <a href="" class="langButton upperNavInfo">
+        <svg class="upperNavIcon" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+        <circle style="fill:#F0F0F0;" cx="256" cy="256" r="256"/>
+        <g>
+        	<path style="fill:#D80027;" d="M244.87,256H512c0-23.106-3.08-45.49-8.819-66.783H244.87V256z"/>
+        	<path style="fill:#D80027;" d="M244.87,122.435h229.556c-15.671-25.572-35.708-48.175-59.07-66.783H244.87V122.435z"/>
+        	<path style="fill:#D80027;" d="M256,512c60.249,0,115.626-20.824,159.356-55.652H96.644C140.374,491.176,195.751,512,256,512z"/>
+        	<path style="fill:#D80027;" d="M37.574,389.565h436.852c12.581-20.529,22.338-42.969,28.755-66.783H8.819
+        		C15.236,346.596,24.993,369.036,37.574,389.565z"/>
+        </g>
+        <path style="fill:#0052B4;" d="M118.584,39.978h23.329l-21.7,15.765l8.289,25.509l-21.699-15.765L85.104,81.252l7.16-22.037
+        	C73.158,75.13,56.412,93.776,42.612,114.552h7.475l-13.813,10.035c-2.152,3.59-4.216,7.237-6.194,10.938l6.596,20.301l-12.306-8.941
+        	c-3.059,6.481-5.857,13.108-8.372,19.873l7.267,22.368h26.822l-21.7,15.765l8.289,25.509l-21.699-15.765l-12.998,9.444
+        	C0.678,234.537,0,245.189,0,256h256c0-141.384,0-158.052,0-256C205.428,0,158.285,14.67,118.584,39.978z M128.502,230.4
+        	l-21.699-15.765L85.104,230.4l8.289-25.509l-21.7-15.765h26.822l8.288-25.509l8.288,25.509h26.822l-21.7,15.765L128.502,230.4z
+        	 M120.213,130.317l8.289,25.509l-21.699-15.765l-21.699,15.765l8.289-25.509l-21.7-15.765h26.822l8.288-25.509l8.288,25.509h26.822
+        	L120.213,130.317z M220.328,230.4l-21.699-15.765L176.93,230.4l8.289-25.509l-21.7-15.765h26.822l8.288-25.509l8.288,25.509h26.822
+        	l-21.7,15.765L220.328,230.4z M212.039,130.317l8.289,25.509l-21.699-15.765l-21.699,15.765l8.289-25.509l-21.7-15.765h26.822
+        	l8.288-25.509l8.288,25.509h26.822L212.039,130.317z M212.039,55.743l8.289,25.509l-21.699-15.765L176.93,81.252l8.289-25.509
+        	l-21.7-15.765h26.822l8.288-25.509l8.288,25.509h26.822L212.039,55.743z"/>
+        </svg>
+        <p>USA</p>
+      </a>
+    </nav>
+    <nav class="navBar">
       <a href="<?php echo get_permalink( wc_get_page_id( 'shop' ) ); ?>" class="navBarLink">INVENTORY</a>
       <a href="" class="navBarLink">PARTS & RACING PRODUCTS</a>
       <a href="" class="navBarLink">THE GARAGE</a>
       <a href="" class="navBarLink">SERVICES</a>
       <a href="" class="navBarLink">CONTACT</a>
-    </div>
+    </nav>
   </header>
