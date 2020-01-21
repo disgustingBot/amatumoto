89a10e3f5f8196e9310e04121e478b67a70d2f22
tomasmparra <tomas.mparra@lattedev.com>
2020-01-21 11:10:14 +0100

    testimonials part 1

---
+++ page-testimonials.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+
+
+
+<main class="">
+
+  <?php
+  $args = array(
+    'post_type'=>'testimonials',
+  );
+  $testimonials=new WP_Query($args);
+  while($testimonials->have_posts()){$testimonials->the_post();?>
+    <quote class="testimonial testimonialCarusel">
+      <h4 class="testimonialAuthor"><?php the_title(); ?></h4>
+      <p class="testimonialQuote"><?php the_content(); ?></p>
+    </quote>
+  <?php } ?>
+
+  <button class="" onclick="plusTesti(-1)">&#10094;</button>
+  <button class="" onclick="plusTesti(+1)">&#10095;</button>
+
+
+
+
+
+
+
+
+
+
+
+</main>
+
+
+
+
+
+
+<?php get_footer(); ?>
