0c1e8ffc5752c652d38fb4eaaac5db4cda890032
tomasmparra <tomas.mparra@lattedev.com>
2020-03-03 17:41:53 +0100

    Update page-contact.php

--- page-contact.php
+++ page-contact.php
@@ -9,7 +9,7 @@
 <?php while(have_posts()){the_post();?>
   <div class="contactMenu">
     <?php wp_nav_menu( array( 'theme_location' => 'contactMenu', 'contactMenu' => 'new_menu_class' ) ); ?>
-    <iframe class="contactMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d95652.41680368959!2d1.8935342063268201!3d41.479607268578874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a48e23c916f48d%3A0x7fe8e79c429372d0!2sMunicipality%20of%20Castellbisbal%2C%20Barcelona!5e0!3m2!1sen!2ses!4v1579828250015!5m2!1sen!2ses" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+    <iframe class="contactMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d47829.97335247507!2d2.053006775603039!3d41.474505263424575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a48e23c916f48d%3A0x7fe8e79c429372d0!2sCastellbisbal%2C+Barcelona!5e0!3m2!1ses!2ses!4v1539783607320&zoom=30000" width="1920" height="250" frameborder="0" style="border:0" allowfullscreen=""></iframe>
   </div>
 
   <main class="contact"><?php the_content();?></main>
