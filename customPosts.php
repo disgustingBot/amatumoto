c73be7c0f0c112611d355ceff96b43ae6f759d90
Rafael Molinero <rafmoline@gmail.com>
2020-03-27 18:55:12 +0100

    New Custom Post

--- customPosts.php
+++ customPosts.php
@@ -114,4 +114,41 @@ function lattedev_custom_posts() {
     'rewrite'            => array( 'slug' => 'loc' ),
   ));
 
+	/* Añado las etiquetas que aparecerán en el escritorio de WordPress */
+$labels = array(
+	'name'               => _x( 'Banners', 'post type general name', 'text-domain' ),
+	'singular_name'      => _x( 'Banner', 'post type singular name', 'text-domain' ),
+	'menu_name'          => _x( 'Banners', 'admin menu', 'text-domain' ),
+	'add_new'            => _x( 'Add new', 'banner', 'text-domain' ),
+	'add_new_item'       => __( 'Add new banner', 'text-domain' ),
+	'new_item'           => __( 'New banner', 'text-domain' ),
+	'edit_item'          => __( 'Edit banner', 'text-domain' ),
+	'view_item'          => __( 'See banner', 'text-domain' ),
+	'all_items'          => __( 'All banners', 'text-domain' ),
+	'search_items'       => __( 'Search banners', 'text-domain' ),
+	'not_found'          => __( 'There are no banners.', 'text-domain' ),
+	'not_found_in_trash' => __( 'No banner in the bin.', 'text-domain' )
+);
+	/* Configuro el comportamiento y funcionalidades del nuevo custom post type */
+$args = array(
+	'labels'             => $labels,
+	'description'        => __( 'Desctription.', 'text-domain' ),
+	'public'             => true,
+	'publicly_queryable' => true,
+	'parent_item_colon'  => true,
+	'show_ui'            => true,
+	'show_in_menu'       => true,
+	'show_in_nav_menus'  => true,
+	'show_in_rest'       => true,
+	'query_var'          => true,
+	'rewrite'            => array( 'slug' => 'banner' ),
+	'capability_type'    => 'post',
+	'has_archive'        => true,
+	'hierarchical'       => true,
+	'menu-icon'          => 'dashicons-palmtree',
+	'menu_position'      => 5,
+	'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'page-attributes', 'revisions', 'custom-fields' )
+);
+register_post_type( 'service', $args );
+
 }
