76f162da8c2b483251bf208152df19c524ae8d8f
tomasmparra <tomas.mparra@lattedev.com>
2020-04-07 13:33:03 +0200

    Update page-about-us.php

--- page-about-us.php
+++ page-about-us.php
@@ -2,7 +2,7 @@
 
 
 <figure class="pageBanner">
-  <img class="pageBannerImg rowcol1" src="<?php echo get_post_thumbnail_id()?>" alt="">
+  <img class="pageBannerImg rowcol1" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt="">
   <figcaption class="pageBannerCaption rowcol1">
     <h2><?php the_title();?></h2>
   </figcaption>
