418bd8ef7bbb1c69c5f1d20be2c5cffb8d3e3102
tomasmparra <50798104+tomasmparra@users.noreply.github.com>
2020-10-02 09:29:37 +0200

    asdf

--- woocommerce/auction-product-template.php
+++ woocommerce/auction-product-template.php
@@ -49,6 +49,12 @@ if ($categories) {
 <article class="singlePage">
   <div class="auctionATFContainer">
     <div class="auctionMediaContainer">
+      <div class="singleSideData auctionSideData onlyMobileG">
+        <?php if (method_exists($product,'get_condition')) { ?>
+          <br>Condition: <?php echo esc_html( $product->get_condition() ); ?>
+        <?php } ?>
+        <?php the_excerpt(); ?>
+      </div>
       <div class="auctionName">
         <?php if($brand){ ?>
           <span class="singleSideAnoMarca auctionMarca"><?php echo $brand; ?></span>
@@ -89,6 +95,18 @@ if ($categories) {
       </div>
     </div>
     <div class='auction-ajax-change auctionDetail' >
+      <?php $video = get_post_meta( $product->id, 'video' )[0]; ?>
+      <?php if($video){ ?>
+        <div class="singleSideVideo onlyMobileG"  onclick="altClassFromSelector('video','#gallery')">
+          <img class="singleSideVideoImg rowcol1" src="https://img.youtube.com/vi/<?php echo $video; ?>/mqdefault.jpg" alt="">
+          <svg class="rowcol1" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 100 100" viewBox="0 0 100 100">
+            <g fill="#fff">
+              <path d="m88.9 30.9c-.4-3.5-3.7-6.9-7.2-7.3-21.1-2.6-42.4-2.6-63.4 0-3.5.4-6.8 3.8-7.2 7.3-1.5 12.8-1.5 25.3 0 38.1.4 3.5 3.7 6.9 7.2 7.3 21.1 2.6 42.4 2.6 63.4 0 3.5-.4 6.8-3.8 7.2-7.3 1.5-12.8 1.5-25.2 0-38.1zm-6.6 37.4c-.1.5-.9 1.4-1.4 1.5-10.2 1.2-20.6 1.9-30.9 1.9s-20.7-.7-30.9-1.9c-.4-.1-1.3-1-1.4-1.5-1.4-12.3-1.4-24.2 0-36.6.1-.5.9-1.4 1.4-1.5 10.2-1.2 20.6-1.9 30.9-1.9s20.7.6 30.9 1.9c.4.1 1.3 1 1.4 1.5 1.4 12.3 1.4 24.3 0 36.6z"/>
+              <path d="m43.3 36.7v26.7l20-13.3z"/>
+            </g>
+          </svg>
+        </div>
+      <?php } ?>
       <?php if ( 'yes' !== $product->get_auction_sealed() ) { ?>
         <h2 class="auctionTitle">Auction info:</h2>
         <p class="auctionDetails">
@@ -184,7 +202,7 @@ if ($categories) {
             step="100" size="<?php echo intval( strlen( $product->get_curent_bid() ) ) + 6; ?>" title="bid"  class="input-text qty  bid text left">
             <input type="button" value="+" class="plus" />
           </div>
-          <button type="submit" class="bid_button button alt"><?php echo wp_kses_post( apply_filters( 'bid_text', esc_html__( 'Bid', 'auctions-for-woocommerce' ), $product ) ); ?></button>
+          <button type="submit" class="bid_button button alt"><?php echo wp_kses_post( apply_filters( 'bid_text', esc_html__( 'Bid NOW', 'auctions-for-woocommerce' ), $product ) ); ?></button>
           <input type="hidden" name="place-bid" value="<?php echo intval( $product_id ); ?>" />
           <input type="hidden" name="product_id" value="<?php echo intval( $product_id ); ?>" />
           <input type="hidden" name="user_id" value="<?php echo intval( get_current_user_id() ); ?>" />
@@ -197,15 +215,14 @@ if ($categories) {
 
   <div class="auctionOtherStuff">
     <div class="acutionOtherStuffAside">
-      <div class="singleSideData">
+      <div class="singleSideData auctionSideData onlyDesktopG">
         <?php if (method_exists($product,'get_condition')) { ?>
           <br>Condition: <?php echo esc_html( $product->get_condition() ); ?>
         <?php } ?>
         <?php the_excerpt(); ?>
       </div>
-      <?php $video = get_post_meta( $product->id, 'video' )[0]; ?>
       <?php if($video){ ?>
-        <div class="singleSideVideo"  onclick="altClassFromSelector('video','#gallery')">
+        <div class="singleSideVideo onlyDesktopG"  onclick="altClassFromSelector('video','#gallery')">
           <img class="singleSideVideoImg rowcol1" src="https://img.youtube.com/vi/<?php echo $video; ?>/mqdefault.jpg" alt="">
           <svg class="rowcol1" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 100 100" viewBox="0 0 100 100">
             <g fill="#fff">
@@ -241,6 +258,29 @@ if ($categories) {
         <a class="singleSideContact" href="<?php echo site_url('auctions-information');  ?>">AUCTION INFO</a>
         <button class="singleSideContact" onclick="altClassFromSelector('alt','#singleRequestInfo')">REQUEST MORE INFO</button>
         <!-- <button class="singleSideContact" onclick="altClassFromSelector('alt','#singleMakeOffer')">MAKE OFFER</button> -->
+      </div>
+      <div class="onlyDesktopF testimonialsContainer <?php echo $clase; ?>">
+        <?php
+        $args = array(
+          'post_type'=>'testimonials',
+          'orderby'=>'rand',
+          'posts_per_page'=>'1'
+        );
+        $testimonials=new WP_Query($args);
+        while($testimonials->have_posts()){$testimonials->the_post();?>
+          <quote class="testimonial">
+            <svg class="testiQuote testiQuote1" width="576" height="448" viewBox="0 0 576 448" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M504 192H448V184C448 161.9 465.9 144 488 144H496C522.5 144 544 122.5 544 96V48C544 21.5 522.5 0 496 0H488C386.5 0 304 82.5 304 184V376C304 415.7 336.3 448 376 448H504C543.7 448 576 415.7 576 376V264C576 224.3 543.7 192 504 192ZM528 376C528 389.2 517.2 400 504 400H376C362.8 400 352 389.2 352 376V184C352 109 413 48 488 48H496V96H488C439.5 96 400 135.5 400 184V240H504C517.2 240 528 250.8 528 264V376ZM200 192H144V184C144 161.9 161.9 144 184 144H192C218.5 144 240 122.5 240 96V48C240 21.5 218.5 0 192 0H184C82.5 0 0 82.5 0 184V376C0 415.7 32.3 448 72 448H200C239.7 448 272 415.7 272 376V264C272 224.3 239.7 192 200 192ZM224 376C224 389.2 213.2 400 200 400H72C58.8 400 48 389.2 48 376V184C48 109 109 48 184 48H192V96H184C135.5 96 96 135.5 96 184V240H200C213.2 240 224 250.8 224 264V376Z" fill="black"/>
+            </svg>
+            <div class="testimonialTxt mainTxtType1">
+              <h4 class="testimonialAuthor"><?php the_title(); ?></h4>
+              <div class="testimonialQuote"><?php the_content(); ?></div>
+            </div>
+            <svg class="testiQuote testiQuote2" width="576" height="448" viewBox="0 0 576 448" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M72 256H128V264C128 286.1 110.1 304 88 304H80C53.5 304 32 325.5 32 352V400C32 426.5 53.5 448 80 448H88C189.5 448 272 365.5 272 264V72C272 32.3 239.7 0 200 0H72C32.3 0 0 32.3 0 72V184C0 223.7 32.3 256 72 256ZM48 72C48 58.8 58.8 48 72 48H200C213.2 48 224 58.8 224 72V264C224 339 163 400 88 400H80V352H88C136.5 352 176 312.5 176 264V208H72C58.8 208 48 197.2 48 184V72ZM376 256H432V264C432 286.1 414.1 304 392 304H384C357.5 304 336 325.5 336 352V400C336 426.5 357.5 448 384 448H392C493.5 448 576 365.5 576 264V72C576 32.3 543.7 0 504 0H376C336.3 0 304 32.3 304 72V184C304 223.7 336.3 256 376 256ZM352 72C352 58.8 362.8 48 376 48H504C517.2 48 528 58.8 528 72V264C528 339 467 400 392 400H384V352H392C440.5 352 480 312.5 480 264V208H376C362.8 208 352 197.2 352 184V72Z" fill="black"/>
+            </svg>
+          </quote>
+        <?php } wp_reset_postdata(); ?>
       </div>
       <div class="superFicha">
         <div>
